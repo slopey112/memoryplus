640adab493a7aac6538d9460c65275b490093b84
slopey <howard_peng@icloud.com>
2021-11-23 23:32:57 -0800

    add basic func for names and faces

--- src/components/face.jsx
+++ src/components/face.jsx
@@ -2,20 +2,9 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 export default class Face extends Component {
-    constructor () {
-        super();
-        this.state = {imageURL: ""};
-        axios.get("http://localhost:5000/api/faces")
-            .then(res => {
-                this.setState({ imageURL: res.data });
-            }).catch(err => {
-                console.log(err);
-            });
-    }
-
     render () {
         return (
-            <img className="h-96 w-96" src={this.state.imageURL} />
+            <img className="h-96 w-96" src={this.props.image} />
         );
     }
 }
--- src/components/names-and-faces.jsx
+++ src/components/names-and-faces.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Face from './face';
 import Random from 'random-name';
+import axios from 'axios';
 
 export default class NamesAndFaces extends Component {
     constructor () {
@@ -8,27 +9,48 @@ export default class NamesAndFaces extends Component {
         let arr = []
         for (let i = 0; i < 50; i++) {
             arr.push({ 
-                image: <Face key={i} />, 
+                image: "",
                 name: Random.first() + " " + Random.last() 
             });
         }
         this.state = { idx: 0, faces: arr };
         this.left = this.left.bind(this);
         this.right = this.right.bind(this);
+        this.getFaceURL = this.getFaceURL.bind(this);
+    }
+
+    componentDidMount () {
+        this.getFaceURL();
     }
 
     left () {
-        this.setState({ idx: this.state.idx - 1 < 0 ? 49 : this.state.idx - 1 });
+        this.setState({ idx: this.state.idx - 1 < 0 ? 49 : this.state.idx - 1 }, this.getFaceURL);
     }
 
     right () {
-        this.setState({ idx: (this.state.idx + 1) % 50 });
+        this.setState({ idx: (this.state.idx + 1) % 50 }, this.getFaceURL);
+    }
+
+    getFaceURL () {
+        if (this.state.faces[this.state.idx].image === "") {
+            axios.get("http://localhost:5000/api/faces")
+                .then(res => {
+                    this.setState({ faces: [
+                        ...this.state.faces.slice(0, this.state.idx),
+                        {
+                            image: res.data,
+                            name: this.state.faces[this.state.idx].name
+                        },
+                        ...this.state.faces.slice(this.state.idx+1)
+                    ]});
+                });
+        }
     }
 
     render () {
         return (
             <div>
-                { this.state.faces[this.state.idx].image } 
+                <Face image={this.state.faces[this.state.idx].image}/>
                 <p>{this.state.faces[this.state.idx].name}</p>
                 <button onClick={this.left}>Left</button>
                 <button onClick={this.right}>Right</button>
