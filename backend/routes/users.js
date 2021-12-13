ebfbd08a5994ef26cde6b2a83b179972da2d85da
slopey <howard_peng@icloud.com>
2021-12-12 16:08:26 -0800

    add login endpoint

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const User = require("../models/user.model");
 const validateRegisterInput = require("../validation/register");
+const validateLoginInput = require("../validation/login");
+
+require("dotenv").config();
 
 router.route("/register").post((req, res) => {
     const { errors, isValid } = validateRegisterInput(req.body);
@@ -31,6 +35,43 @@ router.route("/register").post((req, res) => {
                 });
             });
         }
+    });
+});
+
+router.route("/login").post((req, res) => {
+    const { errors, isValid } = validateLoginInput(req.body);
+    if (!isValid) {
+        return res.status(400).json(errors);
+    }
+    const username = req.body.username;
+    const password = req.body.password;
+    User.findOne({ username }).then(user => {
+        if (!user) {
+            return res.status(404).json("Email not found");
+        }
+        bcrypt.compare(password, user.password).then(isMatch => {
+            if (isMatch) {
+                const payload = {
+                    id: user.id,
+                    name: user.username
+                };
+                jwt.sign(
+                    payload,
+                    process.env.SECRET_OR_KEY,
+                    {
+                        expiresIn: 31556926
+                    },
+                    (err, token) => {
+                        res.json({
+                            success: true,
+                            token: "Bearer " + token
+                        });
+                    }
+                );
+            } else {
+                return res.status(400).json("Password incorrect");
+            }
+        });
     });
 });
 
--- backend/validation/login.js
+++ backend/validation/login.js
@@ -1,4 +1,4 @@
-const Validator = require('validator");
+const Validator = require("validator");
 const isEmpty = require("is-empty");
 
 function validateLoginInput(data) {
