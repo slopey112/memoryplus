0d33dfbc2632f89b4fe35cff65f79d742ac0f018
slopey <howard_peng@icloud.com>
2022-01-06 16:47:17 -0800

    upload sync

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 import axios from 'axios';
 
@@ -16,21 +17,25 @@ export default class Header extends Component {
         } else {
             username = "Guest";
         }
-        this.state = { user: username };
+        this.state = { user: username, authenticated: (user ? true : false) };
         this.handleClick = this.handleClick.bind(this);
     }
 
     handleClick () {
         localStorage.removeItem("jwtToken");
         delete axios.defaults.headers.common["Authorization"];
-        this.setState({ user: "Guest" });
+        this.setState({ user: "Guest", authenticated: false });
     }
 
     render () {
         return (
             <div style={divStyle} className="p-8 flex justify-between bg-blue-600 text-gray-200">
                 <h1 className="my-auto w-36" >Hello, <span className="font-semibold">{this.state.user}</span></h1>
-                <button onClick={this.handleClick} className="font-semibold">Logout</button>
+                {
+                    this.state.authenticated ? 
+                    <button onClick={this.handleClick} className="font-semibold">Logout</button> :
+                    <Link to="/login" className="font-semibold">Login</Link>
+                }
             </div>
         );
     }
